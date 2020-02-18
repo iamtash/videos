c2df9b822adf32cc70cb462b8419a6cda9c6dc27
iamtash <natashachernis@gmail.com>
2020-02-18 13:00:37 -0600

    break .map into two lines

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,9 @@ import React from 'react'
 import VideoItem from './VideoItem'
 
 const VideoList = ({ videos }) => {
-    const renderedList = videos.map(video => <VideoItem key={video.id.videoId} />)
+    const renderedList = videos.map(video => {
+        return <VideoItem key={video.id.videoId} video={video} />
+    })
 
     return <div>{renderedList}</div>
 }
